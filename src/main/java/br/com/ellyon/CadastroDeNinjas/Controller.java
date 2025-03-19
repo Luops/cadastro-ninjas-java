eb731c7f6b7cc9c8c692b53774cdc338c304901b
fabricio rios <fabricio.rioslopes@gmail.com>
2025-03-18 21:41:10 -0300

    Criado a primeira rota de teste.

---
+++ src/main/java/br/com/ellyon/CadastroDeNinjas/Controller.java
@@ -0,0 +1,15 @@
+package br.com.ellyon.CadastroDeNinjas;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // Setar que esse arquivo é um controlador.
+@RequestMapping // Annotation para criar rotas
+public class Controller {
+
+    @GetMapping("boasVindas") // Criar rota ex.: localhost:8080/boasVindas
+    public String boasVindas(){
+        return "Essa é a minha primeira mensagem nessa rota!";
+    }
+}
